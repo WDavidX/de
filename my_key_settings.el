62b841d38df8841495be98f9bc0e4b9fe3cafaed
DavidXuX200 <mysolowing@gmail.com>
2012-09-17 03:47:12 -0500

    eval last sexp in emacs lisp mode

--- my_key_settings.el
+++ my_key_settings.el
@@ -140,11 +140,13 @@ With argument, do this that many times."
 ;; ==================== System Coding ====================
 (setq buffer-file-coding-system 'utf-8-unix)
 (setq default-file-name-coding-system 'utf-8-unix)
-(setq default-keyboard-coding-system 'utf-8-unix)
+(setq default-keyboard-coding-system 'utf-8-unix) ;
 (setq default-sendmail-coding-system 'utf-8-unix)
 (setq default-terminal-coding-system 'utf-8-unix)
 ;; ==================== Add hooks ====================
 (add-hook 'emacs-lisp-mode-hook (lambda () (define-key emacs-lisp-mode-map "\C-\\" 'eval-last-sexp)))
+;; (add-hook 'emacs-lisp-mode-hook (lambda ()					(define-key emacs-lisp-mode-map "\C-\\" (eval-region (line-beginning-position)(line-end-position)))))
+
 ;; ==================== Keyboard Definition ====================
 (global-set-key "\C-x\C-b" 'ibuffer)
 (global-set-key "\C-xk" 'kill-this-buffer)
@@ -225,6 +227,12 @@ With argument, do this that many times."
 ;; (if (eq window-system 'w32) (set-frame-font "Lucida Console 14") )
 ;; (if (eq window-system 'w32) (set-frame-font "Monaco 14") )  ;good
 (if (eq window-system 'w32) (set-frame-font "Monaco 12") )  ;good
+;; Chinese Font
+(if (eq window-system 'w32)
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font)
+                      charset
+                      (font-spec :family "Microsoft Yahei" :size 26)))
 ;; (if (eq window-system 'w32) (set-frame-font "Anonymous 12") ) ;good
 ;; (if (eq window-system 'w32) (set-frame-font "DejaVu Sans Mono 14") )
 ;; (if (eq window-system 'w32) (set-frame-font "Consolas 11") ) ;good
